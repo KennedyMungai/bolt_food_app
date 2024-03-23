8f0b2ac847618cd2bd444f2daa2ec45609f78e54
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-23 18:09:32 +0300

    Added some markup elements and styles to the Tab Home Page

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,11 +1,17 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 
 const TabHomePage = () => {
   return (
-    <View className="flex-1">
-      <Text>TabHomePage</Text>
-    </View>
+    <SafeAreaView className="flex-1 p-4 pt-12 bg-white">
+      <View className="flex-row justify-between">
+        <View className="flex-row items-center gap-2">
+          <MaterialCommunityIcons name="map-marker-outline" size={28} />
+          <Text className="text-lg">Your Address</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
