00bfa61a4c4e9396b86602a09cfc3d97cd5c3341
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-23 18:16:36 +0300

    Added the index route to the layout file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
+      <Stack.Screen name="index" />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
