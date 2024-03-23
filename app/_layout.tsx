84df6146ad47407d16130e7cbb203f98ed4c510f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-23 18:44:19 +0300

    Created and registered the storeDetailsPage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,14 @@ export default function RootLayout() {
     <Stack>
       <Stack.Screen name="index" />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="storeDetails/[storeDetails]"
+        options={{
+          headerTransparent: false,
+          animation: 'slide_from_right',
+          animationDuration: 300,
+        }}
+      />
     </Stack>
   );
 }
