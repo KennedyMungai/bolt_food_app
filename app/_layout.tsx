af63e1c3537abbc606b120936cde82f78263ddec
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-23 20:50:20 +0300

    Set a default route path for the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,12 +4,15 @@ import { Ionicons } from '@expo/vector-icons';
 import { Stack, useNavigation } from 'expo-router';
 import { TouchableOpacity } from 'react-native';
 
+export const unstable_settings = {
+  initialRouteName: '(tabs)/home',
+};
+
 export default function RootLayout() {
   const navigation = useNavigation();
 
   return (
     <Stack>
-      <Stack.Screen name="index" />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen
         name="storeDetails/[storeDetails]"
