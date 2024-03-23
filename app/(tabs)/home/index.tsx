a359fed159960a1975cb864cec346ec9d020b158
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-23 18:41:41 +0300

    Rendered out all the items in the dummy restaurant data list

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,12 +1,13 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, Text, View } from 'react-native';
 
+import { dummyRestaurantsData } from '~/assets/data/restaurantData';
 import MarketingCard from '~/components/MarketingCard';
 
 const TabHomePage = () => {
   return (
-    <SafeAreaView className="flex-1 p-4 pt-12 bg-white">
+    <SafeAreaView className="flex-1 p-4 pt-12 bg-white pb-10">
       <View className="flex-row justify-between">
         <View className="flex-row items-center gap-2">
           <MaterialCommunityIcons name="map-marker-outline" size={28} />
@@ -14,7 +15,17 @@ const TabHomePage = () => {
         </View>
       </View>
 
-      <MarketingCard />
+      <FlatList
+        data={dummyRestaurantsData}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item) => item.id.toString()}
+        ListHeaderComponent={() => (
+          <Text className="text-2xl font-bold text-center text-neutral-600 mt-3">
+            All Restaurants and Stores
+          </Text>
+        )}
+        renderItem={({ item }) => <MarketingCard data={item} />}
+      />
     </SafeAreaView>
   );
 };
